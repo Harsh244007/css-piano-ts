1f16a3437f011948e2a5e5f22bff242b5d964314
Harsh Patel <Patelharsh241999@gmail.com>
2023-08-27 00:37:21 +0530

    * Index.html node_modules package.json public push.sh README.md src tsconfig.json yarn.lock Added First Change. .

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `<div class="wrapper">
+<div class="white-keys">
+<div class="white"></div>
+<div class="white"></div>
+<div class="white"></div>
+<div class="white"></div>
+<div class="white"></div>
+<div class="white"></div>
+<div class="white"></div>
+</div>
+<div class="black-keys">
+<div class="black"></div>
+<div class="black"></div>
+<div class="black empty"></div>
+<div class="black"></div>
+<div class="black"></div>
+<div class="black"></div>
+</div>
+</div>`;
